bef84bb07c4bcec8f9c3818feb523a393d0197ed
Gaetano <gpsapia@gmail.com>
2018-04-11 10:34:16 +0200

    Set e Get

--- src/game/core/Entity.java
+++ src/game/core/Entity.java
@@ -9,4 +9,20 @@ public abstract class Entity extends GameObject {
     	dir_x = 0;
     	dir_y = 0;
     }
+    
+    public int getDirX () {
+    	return dir_x;
+    }
+    
+    public void setDirX (int dir_x) {
+    	this.dir_x = dir_x;
+    	}
+    
+    public int getDirY () {
+    	return dir_y;
+    	}
+    
+    public void setDirY (int dir_y) {
+    	this.dir_y = dir_y;
+        }
 }
