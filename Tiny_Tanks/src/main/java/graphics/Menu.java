7f5ce4c5b7afd0753f1c681c9aec798ec5faf271
rgsax <riccardog.sax@gmail.com>
2018-06-15 11:27:39 +0200

    adesso è possibile scegliere l'indirizzo del server

--- Tiny_Tanks/src/main/java/graphics/Menu.java
+++ Tiny_Tanks/src/main/java/graphics/Menu.java
@@ -5,7 +5,9 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
@@ -15,10 +17,12 @@ public class Menu extends GridPane {
 	Button levelEditorButton = new Button("LEVEL EDITOR");
 	Button hostButton = new Button("HOST GAME");
 	Button multiplayerButton = new Button("MULTIPLAYER");
+	CheckBox useDefaultIP = new CheckBox("use default IP");
+	TextField ipField = new TextField(defaultIP);
 	Text title = new Text("Tiny Tanks");
 	Spinner<Integer> spinner = new Spinner<>(2, 10, 2);
 	WindowManager windowManager;
-	static String defaultIP = "192.168.0.101";
+	static String defaultIP = "192.168.0.100";
 	
 	public Menu(WindowManager windowManager) {
 		this.windowManager = windowManager;
@@ -31,9 +35,12 @@ public class Menu extends GridPane {
 		this.setMinSize(800,  800);
 		this.setPadding(new Insets(100, 100, 100, 100));
 		this.setVgap(50);
+		this.setHgap(50);
 		this.add(title, 0, 0);
 		this.add(playButton, 0, 1);
 		this.add(hostButton, 0, 3);
+		this.add(ipField, 0, 4);
+		this.add(useDefaultIP, 1, 4);
 		this.add(multiplayerButton, 0, 5);
 		this.add(levelEditorButton, 0, 7);
 		this.add(spinner, 1, 3);
@@ -73,6 +80,20 @@ public class Menu extends GridPane {
 				windowManager.scene = new Scene(new Client(defaultIP, 8182));
 				windowManager.initEH();
 				windowManager.stage.setScene(windowManager.scene);
+			}
+		});
+		
+		useDefaultIP.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if(useDefaultIP.isSelected()) {
+					ipField.setDisable(true);
+					ipField.setText(defaultIP);
+				}
+				else {
+					ipField.setDisable(false);
+				}
+					
 			}
 		});
 		
